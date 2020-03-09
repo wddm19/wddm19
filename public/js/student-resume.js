69081fbc27e9cc71c4d3a276e23f1ff1b09c5bb9
Negar <negar.khast@gmail.com>
2020-03-09 11:47:53 -0400

    Add student experience to student page

---
+++ public/js/student-resume.js
@@ -0,0 +1,204 @@
+
+  const student = [
+    {
+        id: 100,
+        image: `/img/student.jpg`,
+        title: `Clara`,
+        lastname: `Khast`,
+        credential:'Diploma',
+        code:`10351`,
+        lenght:4,
+        start: `September`,
+        location: `Lakeshore`,
+        status: `Open`,
+        description: `I’m a junior front-end developer with a good understanding of web design. I am willing to expand my knowledge in
+        web development to become a full-stack developer. I stay updated on all of the technologies associated with this
+        industry and have the ability to adhere to all rules and coding standards set forth by management. I also have a
+        great social and self-management skills which gives me the ability to work both in groups and alone.`,
+        skills: [`UI/UX Design`, `Front-End Development`, `Graphic Design`, `Back-End Development`],
+        career: `In addition to traditional stage, film and television opportunities, graduates of the program qualify to work in a wide range of multimedia including webisodes, virtual reality and gaming videos, as well as voice-overs for cartoons, commercials, narration and audio dialogue replacement (dubbing).`
+    },
+    {
+        id: 101,
+        image: `/img/Hailey.png`,
+        title: `Hailey`,
+        lastname: `Koo`,
+        credential:'Advanced Diploma',
+        code:`AN311`,
+        lenght:6,
+        start: `September`,
+        location: `North`,
+        status: `Open`,
+        description: `In Humber’s Animation 3D advanced diploma program, you will develop a well-rounded knowledge base and skillset that includes strong art, animation, modelling and visualization abilities. You will be prepared for the ever-expanding 3D art field with a thorough understanding of the latest 3D digital art technologies in a program developed with the input of industry advisors. Learn to work like the pros do — using the same 3D animation software and tools to render creative thoughts into compelling images and fully-realized design.`,
+        skills: [`Animation`, `Modeling`, `Rigging`, `Lighting`, `Rendering`, `Compositing`],
+        career: `Pour your creative ideas and your technical skills into a dynamic career in the fast-evolving field of 3D animation where your chances to progress are only as limited as your imagination. The rapid advancement in computer technology, combined with skyrocketing global demand for animated entertainment, cable and satellite TV, and the Internet, has led to a surge in career opportunities for those who have the expertise to tell effective stories and create compelling digital characters and worlds in 3D.`
+    },
+    {
+        id:102,
+        image: `/img/maftuna.png`,
+        title: `Maftuna`,
+        lastname: `Kh`,
+        credential:'Maftuna',
+        code:`11671`,
+        lenght:2,
+        start: `September`,
+        location: `North`,
+        status: `Open`,
+        description: `Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life. You will develop skills in modelling, texturing and lighting, as well as in visual effects, compositing and digital cinematography. We employ a teaching process and course structure that requires content created in one course to be used to complete assignments in other courses. This “pipeline” process mirrors that of industry, allowing students to make a seamless transition into a real production environment.`,
+        skills: [`Modelling`, `Texturing`, `Visual effects`, `Compositing`, `Digital cinematography`],
+        career: `Pour your creative ideas and your technical skills into a dynamic career in the fast-evolving field of 3D animation where your chances to progress are only as limited as your imagination. The rapid advancement in computer technology, combined with skyrocketing global demand for animated entertainment, cable and satellite TV, and the Internet, has led to a surge in career opportunities for those who have the expertise to tell effective stories and create compelling digital characters and worlds in 3D.`
+    }
+]
+
+const studentBtn = event => {
+    let students;
+    if (document.getElementById("act").checked) {
+        students = student.filter(student => student.title == `Clara`);
+    } else if (document.getElementById("animation").checked) {
+        students =student.filter(student => student.title == `Maftuna`);
+    } else if (document.getElementById("3d").checked) {
+        students = student.filter(student => student.title == `Hailey`);
+    } else {
+      return;
+    }
+    renderItemsFromArray(students);
+}
+
+const getItemsAsHtmlString = student => {
+
+    return `
+    <section id="home">
+    <div class="intro-container">
+        <div class="intro-text">
+            <h1>i<span class="green">'</span>m <br>${student.title}<br>${student.lastname}<span class="green">&bull;</span></h1>
+            <p>front-end developer &amp; <br>Web designer</p>
+        </div>
+    </div>
+</section>
+
+<section id="about" class="about">
+<div class="about-wrapper">
+<div class="profile-smallimg"><div class="profile__image"><img src="${student.image}"></div></div>
+
+    <p>${student.description} </p>
+    <input type="button" value="Download resume">
+    </div>
+</section>
+
+<section id="skills" class="clearfix">
+    <section id="skill" class="skills">
+        <h3><span class="number">01</span> my skills</h3>
+        <p>My knowledge level in web development</p>
+    
+      <ul class="skills-list">
+      <li class="skill-percentage">${student.skills[0]}</li>
+      <li class="skill-percentage">${student.skills[1]}</li>
+      <li class="skill-percentage">${student.skills[2]}</li>
+      <li class="skill-percentage">${student.skills[3]}</li>
+    </ul>
+
+    </section>
+</section>
+
+<section id="portfolio" class="skills">
+    <h3><span class="number">02</span> portfolio</h3>
+    <p>some of my projects</p>
+
+    <ul class="img-list">
+        <li>
+            <a href="#" target="_blank">
+                <img src="img/1.jpg" width="230" height="220" />
+                <span class="text-content"><span>Travel Blog</span></span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#" target="_blank">
+                <img src="img/3.jpg" width="230" height="220" />
+                <span class="text-content"><span>E-Commerce</span></span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#" target="_blank">
+                <img src="img/student.jpg" width="230" height="220" />
+                <span class="text-content"><span>WebStore</span></span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#" target="_blank">
+                <img src="img/7.jpg" width="230" height="220" />
+                <span class="text-content"><span>Profit Calculator</span></span>
+            </a>
+        </li>
+
+
+    </ul>
+</section>
+
+<section id="timeline">
+      <h3><span class="number">03</span> Experience</h3>
+      <ul>
+        <li>
+          <div>
+            <time>MAY-SEP 2014</time>
+            <h4 class="company">KTE.CO</h4> 
+            <p class="title">Frontend Developer</p>
+            <p class="description">During my internship, I had a chance to learn about some of the most popular web technologies and develop functional websites based on usability, as a part of team. I was able to achieve a proficient understanding of web markup (HTML5 & CSS3) , client-side scripting and JavaScript frameworks (jQuery).</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <time>MAY-SEP 2015</time>
+            <h4 class="company">KTE.CO</h4> 
+            <p class="title">Web Designer and Frontend Developer</p>
+            <p class="description">I was introduced to web designing during this internship and I was able to design a webpage layout, edit and write some of the contents using Photoshop. I was also responsible for assisting with coding and troubleshooting.</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <time>2011 - 2016</time>
+            <h4 class="company">Metropolia Uas</h4> 
+            <p class="title">Student</p>
+            <p class="description">Studying for Bachelor degree of Information Technology. Through my studies and personal projects, I had the opportunity of learning some of the most popular web technologies.</p>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section id="contact">
+      <div id="intro">
+        <h2>contact</h2>
+        <p>I'm available fo freelance. If you have a project that I can help you with or you are interested in working together contact me right now. </p><br>
+        <p>negarkhast@gmail.com <br>Tel: +358-465653673</p>
+      </div>
+
+      <form id="contact_form">
+        <input type="text" placeholder="Name" id="name" name="name">
+        <input type="email" placeholder="Email" name="email" id="email">
+        <input type="text" placeholder="Subject" id="subject" name="subject"> 
+        <textarea placeholder="Message" name="message" id="message"></textarea>
+        <input type="submit" value="Send" id='send_message'>
+        </p>
+      </form>
+    </section>
+
+    <section id="last-section">
+      <div></div>
+    </section>
+
+   `;
+}
+
+const renderItemsFromArray = arr => {
+    if (arr.length > 0) {
+        document.getElementById('items').innerHTML = arr.map(getItemsAsHtmlString).join('\n');
+    } else {
+        return;
+    }}
+
+window.addEventListener('load', () => {
+    document.getElementById('student').addEventListener('click', studentBtn);
+    renderItemsFromArray(student);
+});
